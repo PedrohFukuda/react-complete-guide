08c9a62614d741d0b349b81cf43762decc76822f
Pedro Fukuda <pedrohfukuda@gmail.com>
2021-06-25 09:07:16 -0400

    joining all projects in one git

---
+++ 05-Hooks/src/components/UI/Input/Input.js
@@ -0,0 +1,36 @@
+import { useRef, useImperativeHandle, forwardRef } from "react";
+import styles from "./Input.module.css";
+
+const Input = forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const focus = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: focus,
+    };
+  });
+
+  return (
+    <div
+      className={`${styles.control} ${
+        props.isValid === false ? styles.invalid : ""
+      }`}
+    >
+      <label htmlFor={props.id}>{props.children}</label>
+      <input
+        ref={inputRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+});
+
+export default Input;
---
+++ 05-Hooks/src/store/auth-context.js
@@ -0,0 +1,41 @@
+import { createContext, useEffect, useState } from "react";
+
+const AuthContext = createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
+});
+
+export default AuthContext;
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const isUserLogged = localStorage.getItem("isLoggedIn");
+    if (isUserLogged === "1") setIsLoggedIn(true);
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
